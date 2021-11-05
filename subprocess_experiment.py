8c0929f0da7313bb3c39bf4553dac568c55c0a11
Drew Young <dmy3070@rit.edu>
2021-11-05 14:58:20 -0400

    Added to subprocess experiment

--- subprocess_experiment.py
+++ subprocess_experiment.py
@@ -1,11 +1,14 @@
 import subprocess
 
 output = subprocess.run('echo "Hello"', shell=True,capture_output=True,text=True) 
+# output = subprocess.run('echo "Hello"', shell=True,stdout=subprocess.PIPE,text=True) 
 '''
 Need to set shell=True for windows
 capture_output is used to save the output to a variable
 text=True will output a string
-stdout=subprocess.PIPE
+stdout=subprocess.PIPE  does same thing as capture output
+stdout=FILENAME will output standard out to a file
+output.stderr will print errors
 '''
 #print(output.stdout.decode()) #use .stdout to print standard output
 print(output.stdout) #use .stdout to print standard output
