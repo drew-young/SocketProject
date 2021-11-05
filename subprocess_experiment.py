0e14edc6323acba12f242e7753fbae574e347d97
Drew Young <dmy3070@rit.edu>
2021-11-05 14:59:29 -0400

    Added to subprocess

--- subprocess_experiment.py
+++ subprocess_experiment.py
@@ -9,6 +9,8 @@ text=True will output a string
 stdout=subprocess.PIPE  does same thing as capture output
 stdout=FILENAME will output standard out to a file
 output.stderr will print errors
+output.returncode will return 0 if all good and 1 if errors
+check=True will check for errors and throw an error to python instead of letting it run
 '''
 #print(output.stdout.decode()) #use .stdout to print standard output
 print(output.stdout) #use .stdout to print standard output
